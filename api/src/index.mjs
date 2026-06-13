f33e8cf0695d2dad7dc2c79420a36a53e3a0c974
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-13 16:10:42 +0200

    separate listener for supertest

--- api/src/index.mjs
+++ api/src/index.mjs
@@ -1,27 +1,5 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import db from "./db.js";
-import nestedRouter from "./routers/nested.js";
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-const apiRouter = express.Router();
-
-// This is an example of how to set up a route. Replace it with your own.
-apiRouter.get("/", async (req, res) => {
-  // Here is an example of making a query to the database you set up:
-  const query = "SELECT 'Hello, world!' AS message;";
-  const result = await db.raw(query);
-  res.json(result);
-});
-
-// Here is an example of optionally setting up nested routes. Replace it or delete as needed.
-apiRouter.use("/nested", nestedRouter);
-
+import app from "./app.mjs";
+  
 app.use("/api", apiRouter);
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
