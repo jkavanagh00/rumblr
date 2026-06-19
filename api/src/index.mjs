54c3bad9d3bf2daf15bec653491407519966bda8
Rashmi Das <rashmidas333@outlook.com>
2026-06-19 10:35:22 +0200

    fixed errors in structure for server and path errors

--- api/src/index.mjs
+++ api/src/index.mjs
@@ -1,6 +1,9 @@
+
+
+import express from "express";
 import app from "./app.mjs";
   
-app.use("/api", apiRouter);
+const PORT = process.env.PORT || 3001;
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
