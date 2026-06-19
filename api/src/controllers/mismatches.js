f808d61c2e063389496733dcb816870731b5f4a2
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-19 15:56:24 +0200

    feat: implement listMismatchesForUser controller to retrieve user mismatches

--- api/src/controllers/mismatches.js
+++ api/src/controllers/mismatches.js
@@ -8,3 +8,19 @@ examples:
 - acceptRumbleRequest
 - rejectRumbleRequest
 */
+
+import { listMismatchesForUser_model } from "../models/mismatches";
+
+
+export async function listMismatchesForUser_controller(req, res, next) {
+    try {
+        const mismatches = await listMismatchesForUser_model(req.user.id);
+
+        if (!mismatches) {
+            return res.status(404).json("No mismatches found")
+        }
+        return res.status(200).json(mismatches);
+    } catch (error) {
+        next(error)
+    }
+}
