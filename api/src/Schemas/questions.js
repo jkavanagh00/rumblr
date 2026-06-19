05c2b8ac2abb8c28862e6f978d3ff8ba45c6fa27
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-19 12:31:48 +0200

    refactor: simplify updateQuestionSchema by reusing createQuestionSchema

--- api/src/Schemas/questions.js
+++ api/src/Schemas/questions.js
@@ -4,7 +4,4 @@ export const createQuestionSchema = z.object({
     content: z.string({ required_error: "Question content is required"})
 });
 
-export const updateQuestionSchema = z.object({
-    id: z.string({required_error: "Question ID is required"}),
-    content: z.string({ required_error: "Updated question content is required"}).min(1, "Updated question content must contain text")
-});
+export const updateQuestionSchema = createQuestionSchema.partial();
