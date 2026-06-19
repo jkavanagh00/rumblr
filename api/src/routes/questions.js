75676f167101a0269ea9fe43010ce2c32f6ba0f5
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-19 14:54:17 +0200

    refactor: reorganize question routes for clarity and consistency

--- api/src/routes/questions.js
+++ api/src/routes/questions.js
@@ -8,13 +8,32 @@ examples:
 */
 
 import express from "express";
-import { createQuestionSchema, updateQuestionSchema } from "../Schemas/questions.js"
-import { addQuestion_controller, getQuestionById_controller, listQuestions_controller, updateQuestion_controller, deleteQuestion_controller } from "../controllers/questions.js";
+import {
+  createQuestionSchema,
+  updateQuestionSchema,
+} from "../Schemas/questions.js";
+import {
+  addQuestion_controller,
+  getQuestionById_controller,
+  listQuestions_controller,
+  updateQuestion_controller,
+  deleteQuestion_controller,
+  getQuestionWithNoResponse_controller
+} from "../controllers/questions.js";
 import { validateBody } from "../middlewares/errors.js";
+import { createResponseSchema } from "../Schemas/response.js";
 const questionsRouter = express.Router();
 
-questionsRouter.post("/", validateBody(createQuestionSchema), addQuestion_controller);
+// get a question that the authenticated user has not responded to
+questionsRouter.get("/", getQuestionWithNoResponse_controller);
 
-questionsRouter.get("/:id", getQuestionsById_controller);
+// get a list of all questions
+questionsRouter.get("/list", listQuestions_controller);
+
+// submit a response
+questionsRouter.post("/:id/respond", validateBody(createResponseSchema), addResponse_controller);
+
+// get question by id
+questionsRouter.get("/:id", getQuestionById_controller);
 
 export default questionsRouter;
