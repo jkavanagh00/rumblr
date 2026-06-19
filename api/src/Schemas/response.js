bbefd613ee65abe631277fb687d93ac0240f952d
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-19 13:25:44 +0200

    refactor: remove user_id and question_id from createResponseSchema, use params and authorised userId instead

--- api/src/Schemas/response.js
+++ api/src/Schemas/response.js
@@ -1,14 +1,6 @@
 import { z } from "zod";
 
 export const createResponseSchema = z.object({
-  user_id: z
-    .string({ required_error: "User ID is required" })
-    .uuid({ message: "Invalid User ID format. Must be a UUID" }),
-    
-  question_id: z
-    .string({ required_error: "Question ID is required" })
-    .uuid({ message: "Invalid Question ID format. Must be a UUID" }),
-    
   agreement_score: z
     .number({ required_error: "Agreement score is required" })
     .int({ message: "Agreement score must be an integer" })
