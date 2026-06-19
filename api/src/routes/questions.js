e09b8809bc84b908183650ff8ee7e7f837c77ea7
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-19 15:14:59 +0200

    feat: add requireAdmin middleware to listQuestions route and include listResponses route

--- api/src/routes/questions.js
+++ api/src/routes/questions.js
@@ -18,22 +18,28 @@ import {
   listQuestions_controller,
   updateQuestion_controller,
   deleteQuestion_controller,
-  getQuestionWithNoResponse_controller
+  getQuestionWithNoResponse_controller,
+  listResponses_controller
 } from "../controllers/questions.js";
 import { validateBody } from "../middlewares/errors.js";
+import { authenticateToken, requireAdmin } from "../middlewares/auth.js"
 import { createResponseSchema } from "../Schemas/response.js";
 const questionsRouter = express.Router();
+questionsRouter.use(authenticateToken);
 
 // get a question that the authenticated user has not responded to
 questionsRouter.get("/", getQuestionWithNoResponse_controller);
 
 // get a list of all questions
-questionsRouter.get("/list", listQuestions_controller);
+questionsRouter.get("/list", requireAdmin, listQuestions_controller);
 
 // submit a response
 questionsRouter.post("/:id/respond", validateBody(createResponseSchema), addResponse_controller);
 
 // get question by id
 questionsRouter.get("/:id", getQuestionById_controller);
+
+// get all responses for the current user
+questionsRouter.get("/responses", listResponses_controller)
 
 export default questionsRouter;
