3de75c3576d93df4f44660abd2a56c534810904e
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-21 13:54:51 +0200

    fix: correct import statement for accountRouter in app.mjs

--- api/src/app.mjs
+++ api/src/app.mjs
@@ -6,7 +6,7 @@ import db from "./database/db.js";
 import authRouter from "./routes/auth.js";
 import chatsRouter from "./routes/chats.js";
 import nestedRouter from "./routes/nested.js";
-import accountsRouter from "./routes/accounts.js";
+import accountRouter from "./routes/account.js";
 // Import global error handler
 import { errorHandler } from "./middlewares/errors.js";
 
