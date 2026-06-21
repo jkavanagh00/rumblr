001876ad21eba8d8466cefd6a8286169673440a7
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-21 15:22:21 +0200

    refactor: move response-related functions to responses.js from statements.js

--- api/src/models/responses.js
+++ api/src/models/responses.js
@@ -30,3 +30,79 @@ export async function fetchSharedResponses_model(user1Id, user2Id, trx = db) {
 
   return sharedResponses;
 }
+
+/**
+ * @param {Object} response - The response data to be inserted into the database.
+ * The response object should contain the following properties:
+ * - statement_id: The ID of the statement being responded to (required).
+ * - user_id: The ID of the user submitting the response (required).
+ * - agreement_score: A numeric score representing the level of agreement with the statement (required).
+ * - importance_score: A numeric score representing the importance of the statement to the user (required).
+ * @param {Object} trx - An optional Knex transaction object. If not provided, the default database connection will be used.
+ * @returns {Promise<Array>} - A promise that resolves to an array containing the ID of the newly inserted response.
+ * @throws {Error} - Throws an error if the database operation fails.
+ */
+
+export async function upsertResponse_model(
+  statementId,
+  userId,
+  payload,
+  trx = db,
+) {
+  const data = {
+    statement_id: statementId,
+    user_id: userId,
+    agreement_score: payload.agreement_score,
+    importance_score: payload.importance_score,
+  };
+
+  const existingResponse = await trx("responses")
+    .select("*")
+    .where("statement_id", statementId)
+    .andWhere("user_id", userId)
+    .first();
+
+  if (existingResponse) {
+    await trx("responses").where("id", existingResponse.id).update(data);
+  } else {
+    await trx("responses").insert(data);
+  }
+  return await trx("responses")
+    .select("*")
+    .where("statement_id", statementId)
+    .andWhere("user_id", userId)
+    .first();
+}
+
+export async function deleteResponse_model(id, trx = db) {
+  const existingResponse = await trx("responses")
+    .select("*")
+    .where("id", id)
+    .first();
+
+  if (!existingResponse) {
+    return undefined;
+  }
+
+  await trx("responses").where("id", id).delete();
+
+  return existingResponse;
+}
+
+export async function listResponses_model(userId, trx = db) {
+  const qb = trx("responses");
+  const responses = await qb.select("*").where("user_id", userId);
+  return responses.length > 0 ? responses : null;
+}
+
+export async function listUsersWhoResponded_model(
+  statementId,
+  excludedUserId,
+  trx = db,
+) {
+  const userIds = await trx("responses")
+    .where("statement_id", statementId)
+    .whereNot("user_id", excludedUserId)
+    .pluck("user_id");
+  return userIds;
+}
