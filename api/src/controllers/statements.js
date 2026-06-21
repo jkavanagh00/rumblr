4b7b473f8f73867ffa97ba8cd426031547d9cad8
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-21 17:57:55 +0200

    refactor: move addResponse_controller and listResponses_controller to responses.js from statements.js

--- api/src/controllers/statements.js
+++ api/src/controllers/statements.js
@@ -66,58 +66,6 @@ export async function getStatementById_controller(req, res, next) {
   }
 }
 
-export async function addResponse_controller(req, res, next) {
-  try {
-    const statementId = req.params.id;
-    const userId = req.user.id;
-    const payload = req.validatedBody;
-    const statement = await getStatementById_model(statementId);
-
-    if (!statement) {
-      return res.status(404).json({ error: "Statement not found" });
-    }
-
-    const response = await db.transaction(async (trx) => {
-      const upsertedResponse = await upsertResponse_model(
-        statementId,
-        userId,
-        payload,
-        trx,
-      );
-
-      const otherUsersWithResponses = await listUsersWhoResponded_model(
-        statementId,
-        userId,
-        trx,
-      );
-
-      for (const otherUserId of otherUsersWithResponses) {
-        await upsertMismatch(userId, otherUserId, trx);
-      }
-      return upsertedResponse;
-    });
-    return res
-      .status(201)
-      .json({ message: "Response submitted successfully", response });
-  } catch (error) {
-    next(error);
-  }
-}
-
-export async function listResponses_controller(req, res, next) {
-  try {
-    const responses = await listResponses_model(req.user.id);
-
-    if (!responses) {
-      return res.status(404).json({ error: "No responses found" });
-    }
-
-    return res.status(200).json(responses);
-  } catch (error) {
-    next(error);
-  }
-}
-
 export async function listStatements_controller(req, res, next) {
   try {
     const statements = await listStatements_model();
