907e42e2a6c28fb7abde87915f8d175eab3bc037
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-21 13:04:00 +0200

    feat: implement hashPassword function for secure password hashing

--- api/src/utils/helpers.js
+++ api/src/utils/helpers.js
@@ -1,3 +1,12 @@
 /*
 all common helper functions should be here
 */
+import bcrypt from "bcrypt";
+
+
+export async function hashPassword(password) {
+    if (typeof password !== "string" || password.length < 8) {
+        throw new Error("Password must be a string of at least 8 characters");
+    }
+    return bcrypt.hash(password, 10);
+}
