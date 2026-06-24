3722d396a44e7fea56f4fafb2e1b1670f945c275
Rashmi Das <rashmidas333@outlook.com>
2026-06-24 11:32:24 +0200

    add user blocking endpoints

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -13,11 +13,15 @@ examples:
 
 import express from "express";
 import { updateUserSchema } from "../Schemas/users.js";
+import { blockParamsSchema } from "../Schemas/block.js";
 import { authenticateToken } from "../middlewares/auth.js";
 import {
   getUser_controller,
   updateUser_controller,
   deleteUser_controller,
+  blockUser_controller,
+  unblockUser_controller,
+  getBlockedUsers_controller,
 } from "../controllers/users.js";
 import { validateBody } from "../middlewares/errors.js";
 
@@ -25,6 +29,19 @@ const router = express.Router();
 
 router.use(authenticateToken);
 
+const validateBlockParams = (req, res, next) => {
+  const result = blockParamsSchema.safeParse(req.params);
+
+  if (!result.success) {
+    return next(result.error);
+  }
+
+  next();
+};
+
+router.get("/blocks", getBlockedUsers_controller);
+router.post("/blocks/:id", validateBlockParams, blockUser_controller);
+router.delete("/blocks/:id", validateBlockParams, unblockUser_controller);
 router.get("/", getUser_controller);
 router.put("/", validateBody(updateUserSchema), updateUser_controller);
 router.delete("/", deleteUser_controller);
