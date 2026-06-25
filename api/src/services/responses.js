00a6e79143ec74e3ba94a4188e6897f5ec73e61a
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-25 17:24:55 +0200

    fix: pass transaction object to getStatementById_model in addResponse_service

--- api/src/services/responses.js
+++ api/src/services/responses.js
@@ -11,7 +11,7 @@ import db from "../database/db.js";
 export async function addResponse_service(userId, statementId, responseData) {
   const response = await db.transaction(async (trx) => {
     
-    const statement = await getStatementById_model(statementId);
+    const statement = await getStatementById_model(statementId, trx);
     if (!statement) {
       throw new Error("No statement with provided id found");
     }
