edc628ca9111ff26e47f168784e815e22db06aa9
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-26 10:58:23 +0200

    fix: load environment variables before defining database client in knex configuration

--- api/src/config/knexfile.js
+++ api/src/config/knexfile.js
@@ -1,10 +1,14 @@
 
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import dotenv from "dotenv";
 
-const client = process.env.DB_CLIENT || "sqlite3";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const client = process.env.DB_CLIENT || "sqlite3";
 
 const knexConfig = {
   client,
