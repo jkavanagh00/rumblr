c3a86f9f563de33c0450a781be99bb918861b16c
jkavanagh00 <jkavanagh00@gmail.com>
2026-06-29 13:17:48 +0200

    Add test lifecycle for rumble termination service

--- api/__tests__/integration/services/rumbles.test.js
+++ api/__tests__/integration/services/rumbles.test.js
@@ -1,6 +1,28 @@
-//todo ("Add tests for rumbles terminate service.");
-
+import { seedRumble } from "../../setup/factories.js";
+import testDb from "../../setup/testDb.js";
 import { jest } from "@jest/globals";
+
+jest.unstable_mockModule("../../../src/database/db.js", () => ({
+  default: testDb,
+}));
+
+const { terminateRumble_service } = await import(
+  "../../../src/services/rumbles.js"
+);
+
+const rumbleId = "11111111-1111-4111-8111-333333333333";
+const requesterId = "11111111-1111-4111-8111-111111111111";
+const receiverId = "22222222-2222-4222-8222-222222222222";
+
+beforeAll(async () => {
+  await testDb.migrate.latest({ directory: "./src/database/migrations" });
+});
+
+beforeEach(async () => {
+  await testDb("rumbles").del();
+  await testDb("rumble_requests").del();
+  await testDb("users").del();
+});
 
 describe("rumble termination service", () => {
   test.todo("Write tests");
